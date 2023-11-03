aae7a1e62b35a2d839ac5795f51a31f3adac73a5
irawrr <ira-koval-2004@mail.ru>
2023-11-03 19:53:24 +1000

    Сделала страницу авторизации главной и добавила вместо кнопки "Редактровать" иконку.

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('home', views.index, name='home'),
     path('create', views.create, name='create'),
     path('delete/<int:pk>/', views.delete, name='delete'),
     path('users', views.users, name='users'),
     path('report', views.report, name='report'),
     path('change', views.change, name='change'),
-    path('log-in', views.login, name='login'),
+    path('', views.login, name='login'),
 ]
