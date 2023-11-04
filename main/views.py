7749e50faaccaeb3fcd3c568f8699cfc161cd52d
LeMelifs <vera_mij_xxx@mail.ru>
2023-11-04 17:12:22 +1000

    Мелкие правки бэка, чтобы кнопки отображались только авторизированному пользователю и доступ ко вкладкам был только у авторизированного пользователя. Чистка кода.

--- main/views.py
+++ main/views.py
@@ -1,33 +1,30 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
 
+@login_required
 def change(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'main/change.html', {'title': 'Текущие задачи', 'tasks': tasks})
 
-
+@login_required
 def report(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'main/report.html', {'title': 'Текущие задачи', 'tasks': tasks})
 
-
-def login(request):
-    tasks = Task.objects.order_by('-id')
-    return render(request, 'main/templates/registration/login.html', {'title': 'Текущие задачи', 'tasks': tasks})
-
-
+@login_required
 def users(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'main/users.html', {'title': 'Пользователи', 'tasks': tasks})
 
-
+@login_required
 def index(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': 'Текущие задачи', 'tasks': tasks})
 
-
+@login_required
 def create(request):
     error = ''
     if request.method == 'POST':
@@ -44,7 +41,7 @@ def create(request):
     }
     return render(request, 'main/create.html', context)
 
-
+@login_required
 def delete(request, pk):
     try:
         obj = Task.objects.get(pk=pk)
